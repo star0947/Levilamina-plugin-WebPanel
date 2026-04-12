4e7c0ec88f1dac5c806574a4e39603d6949f6c49
star <1552339622@qq.com>
2026-04-12 22:21:57 +0800

    update 5 times

--- xmake.lua
+++ xmake.lua
@@ -1,19 +1,11 @@
--- 1. 配置规则和仓库
 add_rules("mode.debug", "mode.release")
-add_repositories("levilamina-repo https://github.com/LiteLDev/xmake-repo.git")
 
--- 2. 添加依赖包
-add_requires("levilamina")
+local sdkdir = get_config("sdk") or "./sdk"
 
--- 3. 定义构建目标
 target("WebPanel")
-    -- 设置为动态库 (.dll / .so)
     set_kind("shared")
-    -- 添加源文件
     add_files("webpanel.cpp")
-    -- 添加依赖包
-    add_packages("levilamina")
-    -- 设置语言标准 (LeviLamina 需要 C++20)
     set_languages("c++20")
-    -- 设置输出文件名
-    set_filename("WebPanel.dll")
+    add_includedirs(sdkdir .. "/include")
+    add_linkdirs(sdkdir .. "/lib")
+    add_links("LLAPI")
